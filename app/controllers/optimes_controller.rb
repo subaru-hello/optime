6f01d4b78aad8d3b390d2f099c3bdb9152b5658c
subaru-hello <octpsubaru@icloud.com>
2021-08-28 11:22:33 +0900

    [add]: optimes_controller crud作成

--- app/controllers/optimes_controller.rb
+++ app/controllers/optimes_controller.rb
@@ -1,22 +1,38 @@
 class OptimesController < ApplicationController
   def index
-  end
-
-  def edit
+    @optimes = Optime.all.includes(:user).order(created_at: :desc)
+    @user = User.find_by(id: params[:id])
   end
 
   def show
+    @optimes = Optime.all.includes(:user).order(created_at: :desc)
+    @user = User.find(params[:id])
+    @optimes = Optime.find(params[:id])
+  end
+  def new
+    @user = User.last
+    @optime = Optime.new
   end
 
   def create
+    @user = User.last
+    @optime= @user.optimes.build(optime_params)
+    if @optime.save
+      redirect_to  result_path ,success:  "#{@optime.title}は投稿されました"
+    else
+      flash[:danger] = "掲示板を作成できませんでした"
+      render :new
+    end
   end
 
-  def update
+  def result
+    @user = User.find_by(id: params[:id])
+    @optime = Optime.find_by(id: params[:id])
   end
+  
+  private
 
-  def destroy
-  end
-
-  def new
+  def optime_params
+    params.require(:optime).permit(:guest_name,:title,:sleep, :eat, :work, :commute, :hygiene)
   end
 end
